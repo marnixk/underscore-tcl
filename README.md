9ee6d7e5e96b7b33ec779a89e9a3d1c6811646e5
Marnix <m.cook@auckland.ac.nz>
2014-03-03 16:53:46 +1300

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 As part of getting to know the Tcl programming language I've created a simple port of the underscorejs library that I'm accustomed to working with. This will make it easier to manipulate lists and arrays in ways I'll understand. 
 
 All functions live in the _ (`underscore`) namespace. 
+
+## Predicates
+
+All closure-like functions that are being used have the following signature:
+
+    proc closure {key val}
+    proc closure {val}
+    
+depending on whether it is an array- or list-based function respectively.
+
+## Functions
+
+See `underscore.tcl` for example uses of all the functions.
+
+### TODO
+
+* will make this into a proper Tcl package
--- underscore.tcl
+++ underscore.tcl
@@ -17,73 +17,76 @@ set a(key2) 5
 set a(key3) 10
 set a(key4) 20
 
-#puts [_::first [list 10 20 30 40]]
-#puts [_::last [list 10 20 30 40]]
-#puts [_::initial [list 10 20 30 40]]
-#
-#set mylist [list 10 20 30 1000 50]
-#set max_element [_::max $mylist]
-#puts "max element: $max_element"
-#
-#set myarray [list { name "Marnix" age 29 } { name "Roeland" age 34 } { name "Saskia" age 36 }]
-#
-#array set oldest [_::max $myarray {
-#    array set local_array $var
-#    return $local_array(age)
-#}]
-#
-#puts "oldest '$oldest(name)'"
-#
-#array set youngest [_::min $myarray {
-#    array set local_array $var
-#    return $local_array(age)
-#}]
-#
-#puts "youngest '$youngest(name)'"
-#
-#set smallest [_::min [list 10 20 304 -100]]
-#puts "Smallest: $smallest"
-#
-#
-#set predicate {
-#    if {$val > 5} then {
-#        return true
-#    }
-#}
-#
-#puts "Full set"
-#parray a
-#puts ""
-#
-#puts "Filter > 5";
-#array unset result
-#array set result [_::filter a $predicate]
-#parray result
-#puts ""
-#
-#puts "Reject > 5";
-#array unset result
-#array set result [_::reject a $predicate]
-#parray result
-#puts ""
-#
-#puts -nonewline "First larger than 5: "
-#puts [_::find a $predicate]
-#
-#if { [_::every a { if {$val < 100} { return true } } ] } {
-#    puts "Everything is smaller than 100"
-#}
-#
-#array set b {a 110 b 90}
-#
-#if { [_::some b { if {$val < 100} { return true } } ] } {
-#    puts "Some are smaller than 100"
-#}
-#
-#if { [_::contains b 100] } then {
-#    puts "110 is in b"
-#}
-#
-#set mapped_list [ _::map a { return [expr {$val * 10}] } ]
-#puts $mapped_list
+puts [_::first [list 10 20 30 40]]
+puts [_::last [list 10 20 30 40]]
+puts [_::initial [list 10 20 30 40]]
+
+set mylist [list 10 20 30 1000 50]
+set max_element [_::max $mylist]
+puts "max element: $max_element"
+
+set myarray [list \
+                { name "Person 1" age 29 } \
+                { name "Person 2" age 34 } \
+                { name "Person 3" age 36 }]
+
+array set oldest [_::max $myarray {
+    array set local_array $var
+    return $local_array(age)
+}]
+
+puts "oldest '$oldest(name)'"
+
+array set youngest [_::min $myarray {
+    array set local_array $var
+    return $local_array(age)
+}]
+
+puts "youngest '$youngest(name)'"
+
+set smallest [_::min [list 10 20 304 -100]]
+puts "Smallest: $smallest"
+
+
+set predicate {
+    if {$val > 5} then {
+        return true
+    }
+}
+
+puts "Full set"
+parray a
+puts ""
+
+puts "Filter > 5";
+array unset result
+array set result [_::filter a $predicate]
+parray result
+puts ""
+
+puts "Reject > 5";
+array unset result
+array set result [_::reject a $predicate]
+parray result
+puts ""
+
+puts -nonewline "First larger than 5: "
+puts [_::find a $predicate]
+
+if { [_::every a { if {$val < 100} { return true } } ] } {
+    puts "Everything is smaller than 100"
+}
+
+array set b {a 110 b 90}
+
+if { [_::some b { if {$val < 100} { return true } } ] } {
+    puts "Some are smaller than 100"
+}
+
+if { [_::contains b 100] } then {
+    puts "110 is in b"
+}
+
+set mapped_list [ _::map a { return [expr {$val * 10}] } ]
+puts $mapped_list
 
